9285de220247da0cdbde37dbeb49f0e275ac6d58
ceylonsullen <hussainaljahmi@gmail.com>
2018-02-13 18:00:12 -0500

    bad stuff

--- Backend/controllers/dbcontroller.js
+++ Backend/controllers/dbcontroller.js
@@ -129,78 +129,44 @@ const requestRecommendations = (req, res) => {
     const bar = new _progress.Bar({}, _progress.Presets.shades_classic);
     const { cart } = req.body;
     
-    // Project.find({ children: { $in: cart } })
-    //     .then(async (proj) => {
-    //         const children = {};
-    //         const some1 = await proj.map((p) => {
-    //             p.children.forEach(child => {
-    //                 children[child] = 0;
-    //             })
-    //         })
-    //         console.log(Object.keys(children).length)
-    //         const some2 = await cart.map((cartid, i) => {
-    //             if (children.hasOwnProperty(cartid)) {
-    //                 delete children[cartid];
-    //                 console.log("deleted ... ", cartid);
-    //             }
-    //         })
-    //         const keyCounter = () => {
-                const children = {};
-                Edge.find({$or: [ { right: {$in: cart}}, {  left: {$in: cart}}]}).sort({weight:-1})
-                    .then((edges) => {
-                        console.log(edges.length, "edges");              
-                        edges.forEach((edge) => {
-                            cart.indexOf(edge.left) === -1 ?
-                            children.hasOwnProperty(edge.left) ? children[edge.left] += edge.weight :children[edge.left] = edge.weight :children.hasOwnProperty(edge.right) ? children[edge.right] += edge.weight :children[edge.right] = edge.weight
-                        })
-                        // if (edges.length > 0) {
-                        //     children[child] /= Object.keys(children).length;
-                        // }
-                    })
-                    .then(()=> {
-                            console.log("children", Object.keys(children).length)
-                            const keysSorted =  Object.keys(children).sort(function(a,b){return children[b]-children[a]})
-                            const keysSliced = keysSorted.slice(0, 300).filter((x) => {
-                                return cart.indexOf(x) < 0;
-                            })
-                            console.log(children[keysSliced[0]], children[keysSliced[keysSliced.length - 1]])
-                            Package.find({_id: { $in: keysSliced}})
-                            .then(pkgs => {
-                                const sortedPkgs =  pkgs.sort(function(a,b){return children[b._id]-children[a._id]})
-                                console.log(children[sortedPkgs[0]._id], children[sortedPkgs[sortedPkgs.length - 1]._id])
-                                return res.json(sortedPkgs);
-                            })
-                            .catch((err) => {
-                                console.log(err)
-                            })
-                    })
-                    .catch((err) => {
-                        console.log(err)
-                    })
-            // }
-            // const promise = new Promise((resolve, reject) => {
-            //     keyCounter();
-            //     console.log("hellow");
-            //     resolve();
-            // });
-            // promise
-            // })
-            // .catch((err) => {
-            //     console.log(err)
-            // })
-            
-        // const refreshIntervalId = setInterval(() => {
-        //     if (Object.keys(children).length > 2) {
-        //         keyCounter(); 
-        //         clearInterval(refreshIntervalId)
-        //     }
-        //     console.log(Object.keys(children).length)
-        // }, 300);
-        // })
-        // .catch((err) => {
-        //     console.log(err)
-        // })
-
+    let arr = cart.map(ele => new mongoose.Types.ObjectId(ele));
+    const cartObj = {}
+    cart.forEach((element) => {
+        cartObj[element] = 0
+    })
+    console.log(arr)
+    const children = {};
+    Edge.find({$or: [ { right: { $in: arr }}, {  left: { $in: arr } } ]}).sort({weight:-1})
+        .then((edges) => {
+            console.log(edges.length, "edges");            
+            edges.forEach((edge) => {
+                if (cartObj.hasOwnProperty(edge.left)) {
+                    children.hasOwnProperty(edge.right) ? children[edge.right] += edge.weight : children[edge.right] = edge.weight
+                } else {
+                    children.hasOwnProperty(edge.left) ? children[edge.left] += edge.weight : children[edge.left] = edge.weight
+                }
+            })
+        })
+        .then(()=> {
+                console.log("children", Object.keys(children).length)
+                const keysSorted =  Object.keys(children).sort(function(a,b){return children[b]-children[a]})
+                const keysSliced = keysSorted.slice(0, 300).filter((x) => {
+                    return cart.indexOf(x) < 0;
+                })
+                console.log(children[keysSliced[0]], children[keysSliced[keysSliced.length - 1]])
+                Package.find({_id: { $in: keysSliced}})
+                .then(pkgs => {
+                    const sortedPkgs =  pkgs.sort(function(a,b){return children[b._id]-children[a._id]})
+                    console.log(sortedPkgs[0].name)
+                    return res.json(sortedPkgs);
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 
@@ -215,10 +181,8 @@ const requestKeyRecommendations = (req, res) => {
             edges.forEach((edge) => {
                 console.log(edge)
                 if (cart.indexOf(edge.left) !== -1) {
-                    console.log(edge.left)
                     children.hasOwnProperty(edge.left) ? children[edge.left] += edge.weight :children[edge.left] = edge.weight
                 } else {
-                    console.log(edge.right)
                     children.hasOwnProperty(edge.right) ? children[edge.right] += edge.weight :children[edge.right] = edge.weight                    
                 }
             })
